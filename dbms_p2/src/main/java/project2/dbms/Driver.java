aebcea1fb3ddb351d16dbdc570fdf64fef44d59b
ky06134 <ky06134@uga.edu>
2023-02-21 19:19:35 -0500

    added 2nd query

--- dbms_p2/src/main/java/project2/dbms/Driver.java
+++ dbms_p2/src/main/java/project2/dbms/Driver.java
@@ -13,8 +13,11 @@ public class Driver {
     static final String PSW = "password";
     public static void main(String[] args) throws SQLException {
         
+        
         String query = "SELECT d.dept_name, COUNT(CASE e.gender WHEN 'F' THEN 1 ELSE NULL END) / COUNT(CASE e.gender WHEN 'M' THEN 1 ELSE NULL END) AS female_male_ratio FROM departments d JOIN dept_emp de ON d.dept_no = de.dept_no JOIN employees e ON e.emp_no = de.emp_no GROUP BY d.dept_name ORDER BY female_male_ratio DESC";
-        Driver.runQuery(query);
+        String query2 = "SELECT e.first_name, e. last_name, e. emp_no, dm. from_date, dm. to_date, if( dm. to_date = '9999-01-01', DATEDIFF(from_date, CURDATE()) * -1/7 , DATEDIFF(from_date, to_date) * -1/7) AS worked FROM dept_manager dm JOIN employees e ON e.emp_no = dm.emp_no ORDER BY worked DESC";
+        Driver.runQuery(query2);
+
         
     } //main
 
